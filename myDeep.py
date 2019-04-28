f9d6c0a1f09d29fdc27ee080814fd4809598a61d
Weizhong Jin <weizhonj@usc.edu>
2019-04-27 23:47:01 -0700

    "Updata"

--- myDeep.py
+++ myDeep.py
@@ -2,19 +2,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE, RandomOverSampler
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.linear_model import LogisticRegression, perceptron, SGDClassifier
 from sklearn.model_selection import cross_val_score,StratifiedKFold
-from sklearn.metrics import accuracy_score,roc_curve,confusion_matrix,precision_recall_curve,auc,roc_auc_score,recall_score,classification_report
+from sklearn.metrics import accuracy_score,roc_curve,confusion_matrix,precision_recall_curve,auc,f1_score,recall_score,classification_report
 from sklearn.metrics import f1_score
 from sklearn import svm
+from sklearn.utils import shuffle
 from sklearn.naive_bayes import GaussianNB,BernoulliNB,ComplementNB
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 def MissingData(data, test_data, method):
     if method == 'method1':
+        data = shuffle(data,random_state = 17)
         train_label = data['class']
         train_data = data.drop('class',axis=1)
         test_label = test_data['class']
@@ -98,22 +100,33 @@ def Feature_selection(feature_choose, train_data_scaler,train_label,test_data_sc
     test_data_selection = selector.transform(test_data_scaler)
     return train_data_selection, test_data_selection
 
-def Balance(balance_method, train_data, train_label):
+def Balance(balance_methods, train_data, train_label):
+    balance_method = balance_methods[0]
+    param = balance_methods[1]
     if balance_method == 'SMOTE':
-        balancer = SMOTE(ratio = 0.5,random_state=17)
+        if param == 1:
+            balancer = SMOTE(sampling_strategy = 'minority',random_state=17)
+            pass
+        else:
+            balancer = SMOTE(ratio = param ,random_state=17)
+            pass
         pass
     else:
+        balancer = RandomOverSampler(sampling_strategy='minority')
         pass
-    print("Imbalance Solution: .{}".format(balance_method))
+    print("Imbalance Solution: .{}".format(balance_methods))
     train_data_final, train_label_final = balancer.fit_sample(train_data, train_label)
     return train_data_final, train_label_final
 
 def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,foldN):
     clf_kind = classifier_parameter[0]
     parameter = classifier_parameter[1:]
+    pN = 19666
+    nN = 19666
     cv = StratifiedKFold(n_splits=foldN)
     cost_vec = []
     score_vec = []
+    cost_rate_vec = []
     if clf_kind == 'svm':
         c_param = parameter[0]
         gamma_param = parameter[1]
@@ -125,6 +138,8 @@ def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,fo
         for c in c_param:
             for gam in gamma_param:
                 for kern in kernel_param:
+                    c1 = 0
+                    c2 = 0
                     cost_final = 0
                     auc_score = 0
                     cost_final = 0  
@@ -137,20 +152,25 @@ def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,fo
                         clf = svm.SVC(C=c,gamma=gam,kernel=kern)
                         clf.fit(train_data_final[train],train_label_final[train])
                         y_pred = clf.predict(train_data_final[val])
-                        Recall = roc_auc_score(train_label_final[val],y_pred)
+                        Recall = f1_score(train_label_final[val],y_pred)
                         auc_score += Recall
                         cm = confusion_matrix(train_label_final[val],y_pred).ravel()
                         cost = 500*cm[2]+10*cm[1]
+                        c1 += cm[1]
+                        c2 += cm[2]
                         cost_final += cost
                     auc_score = auc_score/foldN
                     cost_final=cost_final/foldN
+                    cost_rate = 10*c1/nN+500*c2/pN
+                    cost_rate_vec.append(cost_rate)
                     cost_vec.append(cost_final)
                     score_vec.append(auc_score)
                     c_vec.append(c)
                     kernel_vec.append(kern)
                     gamma_vec.append(gam)
-                    print ('Recall score  =',auc_score)
+                    print ('F1 score  =',auc_score)
                     print('Cost = ' , cost_final)
+                    print('Cost Rate = ', cost_rate)
                     print('-------------------------')
         ind_max = cost_vec.index(min(cost_vec))
         best_c = c_vec[ind_max]
@@ -167,6 +187,8 @@ def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,fo
         output_parameter = [clf_kind]
         for c in c_param:
             for penal in penaltys:
+                c1 = 0
+                c2 = 0
                 auc_score = 0
                 print('------------------------')
                 print("C Parameter :", c)
@@ -177,20 +199,25 @@ def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,fo
                     clf = LogisticRegression(C = c, penalty = penal ,solver='liblinear')
                     clf.fit(train_data_final[train],train_label_final[train])
                     y_pred = clf.predict(train_data_final[val])
-                    Recall = roc_auc_score(train_label_final[val],y_pred)
+                    Recall = f1_score(train_label_final[val],y_pred)
                     auc_score += Recall
                     cm = confusion_matrix(train_label_final[val],y_pred).ravel()
                     cost = 500*cm[2]+10*cm[1]
+                    c1 += cm[1]
+                    c2 += cm[2]
                     cost_final += cost
                 cost_final/foldN
                 cost_final=cost_final/foldN
+                cost_rate = 10*c1/nN+500*c2/pN
+                cost_rate_vec.append(cost_rate)
                 auc_score = auc_score/foldN
                 cost_vec.append(cost_final)
                 score_vec.append(auc_score)
                 c_vec.append(c)
                 penalty_vec.append(penal)
-                print ('Recall score =',auc_score)
-                print('Cost = ' , cost_vec)
+                print ('F1 score =',auc_score)
+                print('Cost = ' , cost_final)
+                print('Cost Rate = ', cost_rate)
                 print('-------------------------')
                 print('')    
         ind_max = cost_vec.index(min(cost_vec))
@@ -207,22 +234,29 @@ def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,fo
             print('------------------------')
             print("Penalty: ", penalty_n)
             cost_final = 0
+            c1 = 0
+            c2 = 0
             for train, val in cv.split(train_data_final, train_label_final):
                 clf = SGDClassifier(loss='perceptron', penalty= penalty_n)
                 clf.fit(train_data_final[train],train_label_final[train])
                 y_pred = clf.predict(train_data_final[val])
-                Recall = roc_auc_score(train_label_final[val],y_pred)
+                Recall = f1_score(train_label_final[val],y_pred)
                 auc_score += Recall
                 cm = confusion_matrix(train_label_final[val],y_pred).ravel()
                 cost = 500*cm[2]+10*cm[1]
+                c1 += cm[1]
+                c2 += cm[2]
                 cost_final += cost
             cost_final=cost_final/foldN
+            cost_rate = 10*c1/nN+500*c2/pN
+            cost_rate_vec.append(cost_rate)
             cost_vec.append(cost_final)
             auc_score = auc_score/foldN
             score_vec.append(auc_score)
             penalty_vec.append(penalty_n)
-            print ('Recall score =',auc_score)
-            print('Cost = ' , cost_vec)
+            print ('F1 score =',auc_score)
+            print('Cost = ' , cost_final)
+            print('Cost Rate = ', cost_rate)
             print('-------------------------')
             print('')    
         ind_max = cost_vec.index(min(cost_vec))
@@ -236,6 +270,8 @@ def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,fo
         for kind in kinds:
             auc_score = 0
             cost_final = 0
+            c1 = 0
+            c2 = 0
             if kind == 'gaussian':
                 clf = GaussianNB()
                 print('GaussianNB:')
@@ -249,16 +285,22 @@ def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,fo
             for train, val in cv.split(train_data_final, train_label_final):
                 clf.fit(train_data_final[train],train_label_final[train])
                 y_pred = clf.predict(train_data_final[val])
-                Recall = roc_auc_score(train_label_final[val],y_pred)
+                Recall = f1_score(train_label_final[val],y_pred)
                 auc_score += Recall
                 cm = confusion_matrix(train_label_final[val],y_pred).ravel()
                 cost = 500*cm[2]+10*cm[1]
                 cost_final += cost
+                c1 += cm[1]
+                c2 += cm[2]
+            cost_rate = 10*c1/nN+500*c2/pN
+            cost_rate_vec.append(cost_rate)
             cost_final=cost_final/foldN
             cost_vec.append(cost_final)
             auc_score = auc_score/foldN
-            print ('Recall score  =',auc_score)
+            score_vec.append(auc_score)
+            print ('F1 score  =',auc_score)
             print('Cost = ' , cost_final)
+            print('Cost Rate = ', cost_rate)
             print('-------------------------')
         ind_max = cost_vec.index(min(cost_vec))
         best_NB = kind[ind_max]
@@ -270,6 +312,8 @@ def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,fo
         k_vec = []
         for k in ks:
             auc_score = 0
+            c1 = 0
+            c2 = 0
             print('------------------------')
             print("K: ", k)
             print('------------------------')
@@ -278,18 +322,23 @@ def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,fo
                 clf = KNeighborsClassifier(n_neighbors= k )
                 clf.fit(train_data_final[train],train_label_final[train])
                 y_pred = clf.predict(train_data_final[val])
-                Recall = roc_auc_score(train_label_final[val],y_pred)
+                Recall = f1_score(train_label_final[val],y_pred)
                 auc_score += Recall
                 cm = confusion_matrix(train_label_final[val],y_pred).ravel()
                 cost = 500*cm[2]+10*cm[1]
                 cost_final += cost
+                c1 += cm[1]
+                c2 += cm[2]
+            cost_rate = 10*c1/nN+500*c2/pN
+            cost_rate_vec.append(cost_rate)
             cost_final=cost_final/foldN
             cost_vec.append(cost_final)
             auc_score = auc_score/foldN
             score_vec.append(auc_score)
             k_vec.append(k)
-            print ('Recall score =',auc_score)
-            print('Cost = ' , cost_vec)
+            print ('F1 score =',auc_score)
+            print('Cost = ' , cost_final)
+            print('Cost Rate = ', cost_rate)
             print('-------------------------')
    
         ind_max = cost_vec.index(min(cost_vec))
@@ -301,26 +350,33 @@ def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,fo
         layer_vec = []
         for layer in layers:
             auc_score = 0
+            c1 = 0
             print('------------------------')
             print("Layers : ", layer)
             print('------------------------')
+            c2 = 0
             cost_final = 0
             for train, val in cv.split(train_data_final, train_label_final):
                 clf = MLPClassifier(hidden_layer_sizes= layer)
                 clf.fit(train_data_final[train],train_label_final[train])
                 y_pred = clf.predict(train_data_final[val])
-                Recall = roc_auc_score(train_label_final[val],y_pred)
+                Recall = f1_score(train_label_final[val],y_pred)
                 auc_score += Recall
                 cm = confusion_matrix(train_label_final[val],y_pred).ravel()
                 cost = 500*cm[2]+10*cm[1]
                 cost_final += cost
+                c1 += cm[1]
+                c2 += cm[2]
+            cost_rate = 10*c1/nN+500*c2/pN
+            cost_rate_vec.append(cost_rate)
             cost_final=cost_final/foldN
             cost_vec.append(cost_final)
             auc_score = auc_score/foldN
             score_vec.append(auc_score)
             layer_vec.append(layer)
-            print ('Recall score =',auc_score)
-            print('Cost = ' , cost_vec)
+            print ('F1 score =',auc_score)
+            print('Cost = ' , cost_final)
+            print('Cost Rate = ', cost_rate)
             print('-------------------------')
    
         ind_max = cost_vec.index(min(cost_vec))
@@ -331,7 +387,7 @@ def Find_Best_Param(classifier_parameter, train_data_final, train_label_final,fo
         output_parameter.append(0.01)
         output_parameter.append(0.01)
         output_parameter.append('rbf')
-    return output_parameter,cost_vec
+    return output_parameter,cost_vec,cost_rate_vec,score_vec
 
 
 
@@ -359,13 +415,13 @@ def Classifier(classifier_parameter, train_data_final, train_label_final, text_d
 
         clf_fit = clf.fit(train_data_final, train_label_final)
         y_pred_test = clf.predict(text_data_final)
-        recall_test_per=roc_auc_score(test_label,y_pred_test)
+        recall_test_per=f1_score(test_label,y_pred_test)
         final += recall_test_per
         cm = confusion_matrix(test_label,y_pred_test).ravel()
         cm_final += cm
     final_score = final/loop
     cm_final = cm_final/loop
-    print ('Final Recall score for Classifier (.{})= .{}'.format(classifier_parameter,final_score))
+    print ('Final F1 score for Classifier (.{})= .{}'.format(classifier_parameter,final_score))
     print('-------------------------')
     print('')
     print('```')
--- train.py
+++ train.py
@@ -14,16 +14,19 @@ missing_data_methods = ['method1','method2']
 preprocess_methods = [['standard','pca'],['minmax','KBest']]
 scaler_method = ['standard', 'minmax']
 feature_choose = ['pca', 'KBest']
-balance_method = 'SMOTE'
-classifier_parameter = ['svm',[0.01,0.1,1,10,100],[0.01],['rbf']] #classifier 1
+balance_method = ['SMOTE', 1 ]
+classifier_parameter = ['svm',[0.01,0.1,1,10,100],[0.01],['rbf','sigmoid']] #classifier 1
 #classifier_parameter = ['SGDperceptron',['l1','l2']]
 #classifier_parameter =['logisticRegression',[0.1,1,10,100],['l2']]
 #classifier_parameter = ['KNN',[2,3,4,5]]
-#classifier_parameter = ['NN',[50,75,100]]
+#classifier_parameter = ['NN',[50,100]]
+#classifier_parameter =['NB',['gaussian','ber']]
 foldN = 5
 loop = 3
 cost_vector = []
-param = [0.5,3]
+cost_rate_vector = []
+score_vector = []
+param = [0.95,60]
 params = np.arange(0.1,0.98,0.02)
 print('Start...')
 data = pd.read_csv("/Users/weizhongjin/usc/ee559/finaldata/aps_failure_training_set_SMALLER.csv" , na_values='na')
@@ -43,11 +46,15 @@ for missing_data_method in missing_data_methods:
         test_data = np.array(test_data)
         train_data, test_data = md.Scaler(scaler_method,train_data,test_data)
         train_data, test_data = md.Feature_selection(feature_choose, train_data,train_label, test_data,param)
-        train_data, train_label = md.Balance(balance_method,train_data,train_label)
-        final_classifier_parameter,cost_vec = md.Find_Best_Param(classifier_parameter, train_data, train_label,foldN)
+        train_data, train_label = md.Balance(balance_method,train_data,train_label)  
+        final_classifier_parameter,cost_vec,cost_rate_vec,score_vec = md.Find_Best_Param(classifier_parameter, train_data, train_label,foldN)
         cost_vector.append(cost_vec)
+        cost_rate_vector.append(cost_rate_vec)
+        score_vector.append(score_vec)
         print('Best classifier parameter :{}'.format(final_classifier_parameter))
         print('Cost vector: {}'.format(cost_vec))
+        print('Cost Rate vector: {}'.format(cost_rate_vec))
+        print('Score vector: {}'.format(score_vec))
         print('/---------------------------------------/')
 #------------------------Don't Use!!!!!!!!!!!!!!!---------------------   
 #------------------------Don't Use!!!!!!!!!!!!!!!---------------------  
@@ -63,4 +70,4 @@ for missing_data_method in missing_data_methods:
 # print('The parameter is : {}'.format_map(params(np.argmin(cost_vector))))
 
 # plt.show()
-print('Final Cost Table: {}'.format(cost_vector))
+print('Final Cost rate Table: {}'.format(cost_rate_vector))
